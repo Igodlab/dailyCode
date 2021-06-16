f20b2ee4706b1e6b87864f526f0668849646f56b
Igodlab <igodlab@gmail.com>
2021-06-16 14:17:02 -0700

    Jun 15: decide whether a list can be ordered (ascending) by moving only one element

---
+++ 2021Jun16.hs
@@ -0,0 +1,30 @@
+-- TechLead daily question Jun 16, 2021
+--
+-- You are given an array of integers in an arbitrary order. Return whether or not it is possible to make the array non-decreasing by modifying at most 1 element to any value.
+--
+-- We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+-- 
+-- Example:
+-- 
+-- [13, 4, 7] should return true, since we can modify 13 to any value 4 or less, to make it non-decreasing.
+
+-- [13, 4, 1] however, should return false, since there is no way to modify just one element to make the array non-decreasing.
+
+{-
+check :: (Num a, Ord a) => [a] -> Bool
+check x
+    | (True `elem` map (< 1) x) = error "Values in list have to be greater than 1"
+    | otherwise = case f of
+        []     -> True
+        (x:[]) -> True
+        (x:_)  -> False
+      where 
+        f (x:xs) = 
+            | filter 
+-}
+
+check :: (Ord a) => [a] -> [a]
+check [] = []
+check (x:xs)
+    | sum([1 | i <- xs, i>x]) > 1 = x:check xs
+    | otherwise = check xs
