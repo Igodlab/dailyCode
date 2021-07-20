4a189f464a519bc7925eb5148e211661736acdc5
Igodlab <igodlab@gmail.com>
2021-07-20 14:15:36 -0700

    Jul 19: check if a binary tree is a Binary SEARCH Tree

---
+++ 2021Jul19.hs
@@ -0,0 +1,24 @@
+-- You are given the root of a binary search tree. Return true if it is a valid binary search tree, and false otherwise. Recall that a binary search tree has the property that all values in the left subtree are less than or equal to the root, and all values in the right subtree are greater than or equal to the root.
+-- 
+-- Here's a starting point:
+-- 
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq, Read, Ord)
+
+-- create the Tree
+
+tree1 = Node 5 (Node 3 (Node 1 Empty Empty) (Node 4 Empty Empty)) (Node 7 (Node 6 Empty Empty) Empty)
+tree2 = Node 5 (Node 3 (Node 1 Empty (Node 0 Empty Empty)) (Node 4 Empty Empty)) (Node 7 (Node 6 Empty Empty) Empty)
+
+is_bst :: (Ord a) => (Tree a) -> Bool
+is_bst Empty = True
+is_bst t = lft t && rgt t
+  where
+    lft (Node x left _) = case left of
+        Empty         -> True
+        (Node xs _ _) -> if x < xs then False else is_bst left
+      
+    rgt (Node x _ right) = case right of
+        Empty         -> True
+        (Node xs _ _) -> if x > xs then False else is_bst right
+
