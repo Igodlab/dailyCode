f4377e7f53f111707230e497d5306d6226866bc3
Igodlab <igodlab@gmail.com>
2022-12-07 21:10:42 -0800

    Sum two elements in list solved

--- 2022/2022Dec07.hs
+++ 2022/2022Dec07.hs
@@ -13,16 +13,22 @@ import Control.Applicative (liftA2)
 -- sophisticated solution using liftA2 
 sumFunction :: [Int] -> Int -> Either [Int] [Int]
 sumFunction [] _ = Left []
-sumFunction xs n = case filtered of
+sumFunction xs n = case filtered (allCombinations [] xs) of
                     [] -> Left []
-                    [(x,y):_]  -> Right [x, y]
+                    (x:_)  -> Right [fst x, snd x]
   where
-    combinationExpand :: [a] -> [a] -> [a]
-    combinationExpand _ [] = []
-    combinationExpand acc (x:xs) = combinationExpand (acc ++ [(x, xi) | xi <- xs]) xs
+    combinations :: [Int] -> [(Int, Int)]
+    combinations [] = []
+    combinations (x:xs) = [(x, xi) | xi <- xs]
 
-    filtered = filter (\(x, y) -> x + y == n) $ combinationExpand [] xs
+    allCombinations :: [(Int, Int)] -> [Int] -> [(Int, Int)]
+    allCombinations acc [] = acc
+    allCombinations acc xt@(x:xs) = allCombinations (acc ++ combinations xt) xs
 
-
-
-
+    filtered :: [(Int, Int)] -> [(Int, Int)]
+    filtered ys = filter (\(p, q) -> p + q == n) ys
+    
+-- try:
+--
+-- Prelude> sumFunction [5, -4, 8, 11, 1, -1, 6, 2, 14] 10
+-- Right [-4,14]
